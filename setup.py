2894f0cf32f3a6950e0caf7523a5679bd2c2944d
Morgan Joyce <morganj2k@gmail.com>
2024-10-21 18:17:12 -0500

    chore(release): prepare v1.0.0 release

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='notion_automation',  # Package name (used in `pip install`)
-    version='0.1.0',  # Follow semantic versioning
+    name='notion_automation',
+    version='1.0.0',
     description='Automate Notion database creation with JSON schemas',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Your Name',
     author_email='your.email@example.com',
-    url='https://github.com/yourusername/notion-automation',  # GitHub repo link
-    packages=find_packages(),  # Automatically detect packages
+    url='https://github.com/yourusername/notion-automation',
+    packages=find_packages(),
     install_requires=[
         'requests',
         'python-dotenv',
